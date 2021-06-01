b4d4f8269e9725e750400064c1b3c6036e631ada
zingimmick <zingimmick@outlook.com>
2021-06-01 14:28:38 +0800

    Support GuzzleHttp 7

--- Obs/Internal/Common/SdkCurlFactory.php
+++ Obs/Internal/Common/SdkCurlFactory.php
@@ -34,7 +34,7 @@ class SdkCurlFactory implements CurlFactoryInterface
         $this->maxHandles = $maxHandles;
     }
 
-    public function create(RequestInterface $request, array $options)
+    public function create(RequestInterface $request, array $options): EasyHandle
     {
         if (isset($options['curl']['body_as_string'])) {
             $options['_body_as_string'] = $options['curl']['body_as_string'];
@@ -78,7 +78,7 @@ class SdkCurlFactory implements CurlFactoryInterface
     	}
     }
 
-    public function release(EasyHandle $easy)
+    public function release(EasyHandle $easy): void
     {
         $resource = $easy->handle;
         unset($easy->handle);
