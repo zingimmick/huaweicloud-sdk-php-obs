6c4ee7bbffb1e3fcf182d07166896ef5263e857b
zingimmick <zingimmick@outlook.com>
2023-05-23 21:24:32 +0800

    Support psr/http-message 2.0

--- Obs/Internal/Common/CheckoutStream.php
+++ Obs/Internal/Common/CheckoutStream.php
@@ -33,7 +33,7 @@ class CheckoutStream implements StreamInterface
         $this->expectedLength = $expectedLength;
     }
 
-    public function getContents()
+    public function getContents(): string
     {
         $contents = $this->stream->getContents();
         $length = strlen($contents);
@@ -43,7 +43,7 @@ class CheckoutStream implements StreamInterface
         return $contents;
     }
 
-    public function read($length)
+    public function read($length): string
     {
         $string = $this->stream->read($length);
         if ($this->expectedLength !== null) {
